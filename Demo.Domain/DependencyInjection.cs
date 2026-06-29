c821b1323fcb9b6681b663acb5c4a6d782dd6f6a
Shalom Achikasim <shalomachims@gmail.com>
2026-06-29 15:47:46 +0100

    feat(chore): setup clean architecture layers and project references

---
+++ Demo.Domain/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Domain
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDomainDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ clean-architecture-demo-v1/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Demo.Application;
+using Demo.Infrastructure;
+
+namespace clean_architecture_demo_v1
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
